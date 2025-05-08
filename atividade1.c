fd5f0d8ea8702cfe3449e674662c2cbeb2ff7387
Alex Henriques <alexhenriques417@gmail.com>
2025-05-08 15:43:29 -0300

    Exercicio sobre coordenadas.
    Problema proposto de captura de dados para coordenadas nos pontos x e y.

---
+++ atividade1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    int x;
+    int y;
+} Ponto;
+
+int main() {
+    Ponto ponto1, ponto2;
+    int distManhattan;
+
+    printf("Digite as coordenadas do primeiro ponto (x, y): ");
+    scanf("%d %d", &ponto1.x, &ponto1.y);
+
+    printf("Digite as coordenadas do segundo ponto (x, y): ");
+    scanf("%d %d", &ponto2.x, &ponto2.y);
+
+    distManhattan = abs(ponto1.x - ponto2.x) + abs(ponto1.y - ponto2.y);
+
+    printf("\nPonto 1: (%d, %d)\n", ponto1.x, ponto1.y);
+    printf("Ponto 2: (%d, %d)\n", ponto2.x, ponto2.y);
+    printf("Distancia de Manhattan: %d\n", distManhattan);
+
+    return 0;
+}
+ 
